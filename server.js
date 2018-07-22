d58e6893367a914528879a845548ec6d8451864f
Nandeep Nagarkar <nandeep@gmail.com>
2018-07-22 00:22:36 -0700

    server.js corrected

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var app = express();
-var PORT = process.eventNames.PORT || 3000;
+var PORT = process.env.PORT || 3000;
 
-app.get('/', function(req,res) {
-    res.send('TODO API Root');
-}); 
+
+
+app.get('/', function (req, res) {
+	res.send('Todo API Root');
+});
 
 app.listen(PORT, function(){
     console.log('Express listening on port ' + PORT +' !' );
