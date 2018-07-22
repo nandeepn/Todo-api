387b901482dc719bc141fdaa3fdc4f608c404f5c
Nandeep Nagarkar <nandeep@gmail.com>
2018-07-22 13:19:57 -0700

    Add underscore data validations

--- server.js
+++ server.js
@@ -34,7 +34,10 @@ app.get('/todos/:id', function (req, res) {
 });
 // POST /todos
 app.post('/todos', function (req, res) {
-	var body = req.body;
+    var body = req.body;
+    if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+        return res.status(400).send();
+    }
 
 	// add id field
 	body.id = todoNextId++;
